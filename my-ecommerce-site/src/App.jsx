5075760e10d0d4658332dbf8a78e5e4b8eab2bf4
JaredP872 <jared.proctor7642@gmail.com>
2025-02-07 12:10:36 -0500

    Added comments

--- my-ecommerce-site/src/App.jsx
+++ my-ecommerce-site/src/App.jsx
@@ -1,3 +1,4 @@
+// Import the BrowserRouter, Routes, and Route components from the react-router-dom package.
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -5,6 +6,7 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
 
+// Define the App component that renders the Navbar, Routes, and Footer components.
 function App() {
   return (
     <Router>
@@ -20,4 +22,5 @@ function App() {
   );
 }
 
+// Export the App component as the default export of this file.
 export default App;
